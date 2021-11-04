aa818c0e8daba5a75315bb351f1a76dba51177d7
simonskarbo <simonskarbo@158-37-236-162.hvl.no>
2021-11-04 15:40:54 +0100

    Oppgave 2
    Tekst

--- src/no/hvl/dat100/jplab11/oppgave2/Tekst.java
+++ src/no/hvl/dat100/jplab11/oppgave2/Tekst.java
@@ -5,8 +5,7 @@ import no.hvl.dat100.jplab11.oppgave1.*;
 
 public class Tekst extends Innlegg {
 
-	protected String tekst;
-	protected Innlegg innlegg;
+	private String tekst;
 	
 	public Tekst () {
 		
@@ -14,26 +13,37 @@ public class Tekst extends Innlegg {
 	
 	public Tekst(int id, String bruker, String dato, String tekst) {
 		
-		innlegg = new Innlegg();
+		super(id, bruker, dato);
+		this.tekst = tekst;
 		
 	}
 	
 	public Tekst(int id, String bruker, String dato, int likes, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
+		
+		super(id, bruker, dato, likes);
+		this.tekst = tekst;
+		
 	}
 	
 	public String getTekst() {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		return tekst;
 
 	}
 
 	public void setTekst(String tekst) {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		this.tekst = tekst;
+		
 	}
 
 	@Override
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		String mid;
+		
+		mid = "TEKST\n" + super.toString() + tekst + "\n";
+		return mid;
 
 	}
 	
